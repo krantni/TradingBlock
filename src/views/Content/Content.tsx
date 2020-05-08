0d8473926bf417d0e2189b5ffbd063cfec991479
krantni <krantni@gmail.com>
2020-05-07 20:44:38 -0500

    tab switching

--- src/views/Content/Content.tsx
+++ src/views/Content/Content.tsx
@@ -7,20 +7,23 @@ const Content: React.FC = () => {
   const {
     data: { teamOwners },
   } = useAppContext();
+
+  const [selectedView, changeView] = React.useState<'trading' | 'history'>('trading');
+
   return (
     <div className={styles.contentContainer}>
       <div className={styles.tabs}>
         <span className={styles.flexGrow1}></span>
-        <div className={styles.tab}>
+        <div className={selectedView === 'trading' ? styles.selectedTab : styles.tab} onClick={() => changeView('trading')}>
           <h3>Trading Block</h3>
         </div>
         <span className={styles.flexGrow2}></span>
-        <div className={styles.tab}>
+        <div className={selectedView === 'history' ? styles.selectedTab : styles.tab} onClick={() => changeView('history')}>
           <h3>Player History</h3>
         </div>
         <span className={styles.flexGrow1}></span>
       </div>
-      <TradingBlock teamOwners={teamOwners} />
+      {selectedView === 'trading' && <TradingBlock teamOwners={teamOwners} />}
     </div>
   );
 };
